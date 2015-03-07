d8592d7eac45d8cb86428381d7ce138abdb12d90
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-03-08 00:50:27 +0900

    add Sample Project for Lombok

---
+++ LibrarySample/LombokSample/app/src/main/java/m/k/android/librarysample/lomboksample/MainActivity.java
@@ -0,0 +1,14 @@
+package m.k.android.librarysample.lomboksample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
