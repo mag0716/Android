9bd0d96ca740979d3cc2da601517e937b4bcd4a5
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-03-29 22:21:12 +0900

    サンプル用のレイアウト、モデルクラス追加

--- LibrarySample/OkHttpSample/app/src/main/java/m/k/android/librarysample/okhttpsample/MainActivity.java
+++ LibrarySample/OkHttpSample/app/src/main/java/m/k/android/librarysample/okhttpsample/MainActivity.java
@@ -2,38 +2,25 @@ package m.k.android.librarysample.okhttpsample;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private TextView mResultText;
+    private ImageView mImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResultText = (TextView)findViewById(R.id.result_text);
+        mImage = (ImageView)findViewById(R.id.image);
     }
 
+    public void onClick(View view) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ LibrarySample/OkHttpSample/app/src/main/java/m/k/android/librarysample/okhttpsample/model/SysJson.java
@@ -0,0 +1,17 @@
+package m.k.android.librarysample.okhttpsample.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+/**
+ * Created by kishimotomasashi on 2015/03/29.
+ */
+@Value
+public class SysJson {
+    private String country;
+    private int id;
+    private double message;
+    private long sunrise;
+    private long sunset;
+    private int type;
+}
