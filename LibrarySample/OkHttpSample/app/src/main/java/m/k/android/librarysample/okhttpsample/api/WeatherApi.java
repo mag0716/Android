c3648b9e17ae2ec5c293855d4415f6f638923925
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-04-04 02:08:56 +0900

    コメント追加

--- LibrarySample/OkHttpSample/app/src/main/java/m/k/android/librarysample/okhttpsample/api/WeatherApi.java
+++ LibrarySample/OkHttpSample/app/src/main/java/m/k/android/librarysample/okhttpsample/api/WeatherApi.java
@@ -18,10 +18,31 @@ public class WeatherApi {
                                       @NonNull String city,
                                       @NonNull Callback callback) {
 
+        // GET API
         Request request = new Request.Builder()
                 .url(String.format(WEATHER_API_URL, city, country))
                 .get()
                 .build();
+
+        // POST API
+        // Request#post 呼び出し、パラメータを渡す
+
+        // Header の設定
+        // Request#header or Request#headers or Request#addHeader
+
+        // キャッシュ
+        // OkHttpClient#setCache
+
+        // タイムアウト
+        // OkHttpClient#setConnectTimeout, OkHttpClient#setWriteTimeout, OkHttpClient#setReadTimeout
+
+        // リトライ
+        // Interceptor を実装して、OkHttpClient#interceptors().add する
+        // https://github.com/square/okhttp/wiki/Interceptors
+
+
+        // TODO: Interceptor サンプル
+
         client.newCall(request).enqueue(callback);
     }
 }
