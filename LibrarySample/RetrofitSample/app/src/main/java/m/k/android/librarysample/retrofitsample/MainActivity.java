54eb35e9409108034ecb643d8a9acaf01ee19ab2
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-04-05 13:22:08 +0900

    add model class for sample

--- LibrarySample/RetrofitSample/app/src/main/java/m/k/android/librarysample/retrofitsample/MainActivity.java
+++ LibrarySample/RetrofitSample/app/src/main/java/m/k/android/librarysample/retrofitsample/MainActivity.java
@@ -4,36 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mText = (TextView)findViewById(R.id.result_text);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btn1:
+                executeSingleApi();
+                break;
+            case R.id.btn2:
+                executeMultiApi();
+                break;
+            case R.id.btn3:
+                executeFailApi();
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void executeSingleApi() {
+
+    }
+
+    private void executeMultiApi() {
+
+    }
+
+    private void executeFailApi() {
+
     }
 }
---
+++ LibrarySample/RetrofitSample/app/src/main/java/m/k/android/librarysample/retrofitsample/model/CoordJson.java
@@ -0,0 +1,12 @@
+package m.k.android.librarysample.retrofitsample.model;
+
+import lombok.Value;
+
+/**
+ * Created by kishimotomasashi on 2015/03/29.
+ */
+@Value
+public class CoordJson {
+    private double lat;
+    private double lon;
+}
