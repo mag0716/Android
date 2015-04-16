20b07701329a3fa0112226f4896c99949a38902a
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-04-16 21:39:33 +0900

    add use ButterKnife in ListView sample.

--- LibrarySample/ButterKnifeSample/app/src/main/java/m/k/android/librarysample/butterknifesample/ListViewSampleActivity.java
+++ LibrarySample/ButterKnifeSample/app/src/main/java/m/k/android/librarysample/butterknifesample/ListViewSampleActivity.java
@@ -1,38 +1,95 @@
 package m.k.android.librarysample.butterknifesample;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnItemClick;
+import butterknife.OnItemSelected;
 
 
 public class ListViewSampleActivity extends ActionBarActivity {
+
+    private static List<String> mDataList;
+
+    static {
+        mDataList = new ArrayList<>();
+        for(int i=0; i<10; i++) {
+            mDataList.add("Data" + i);
+        }
+    }
+
+    @InjectView(R.id.list)
+    ListView mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_sample);
+        ButterKnife.inject(this);
+
+        Adapter adapter = new Adapter(this, R.layout.list_item);
+        adapter.addAll(mDataList);
+        mList.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list_view_sample, menu);
-        return true;
+    @OnItemClick(R.id.list)
+    void showData(int position) {
+        Toast.makeText(this, mList.getAdapter().getItem(position).toString(), Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public static class Adapter extends ArrayAdapter<String> {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        static class ViewHolder {
+            @InjectView(R.id.text1)
+            TextView mText1;
+            @InjectView(R.id.text2)
+            TextView mText2;
+
+            public ViewHolder(View view) {
+                ButterKnife.inject(this, view);
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+        private int mResourceId;
+
+        public Adapter(@NonNull Context context, @LayoutRes int resource) {
+            super(context, resource);
+            mResourceId = resource;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView != null) {
+                holder = (ViewHolder) convertView.getTag();
+            } else {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(mResourceId, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }
+
+            final String data = getItem(position);
+            holder.mText1.setText("Title: " + data);
+            holder.mText2.setText("Sub: " + data);
+
+            return convertView;
+        }
     }
 }
