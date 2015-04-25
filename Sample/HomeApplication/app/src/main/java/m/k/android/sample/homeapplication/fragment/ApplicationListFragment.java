1d6bcd2b35bbbdd7eadf6bdede6fc690e1bd4b35
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-04-25 23:55:23 +0900

    add  - show application list.

--- Sample/HomeApplication/app/src/main/java/m/k/android/sample/homeapplication/fragment/ApplicationListFragment.java
+++ Sample/HomeApplication/app/src/main/java/m/k/android/sample/homeapplication/fragment/ApplicationListFragment.java
@@ -1,50 +1,35 @@
 package m.k.android.sample.homeapplication.fragment;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import m.k.android.sample.homeapplication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ApplicationListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ApplicationListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ApplicationListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @InjectView(R.id.grid)
+    GridView mGrid;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ApplicationListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ApplicationListFragment newInstance(String param1, String param2) {
-        ApplicationListFragment fragment = new ApplicationListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private List<ResolveInfo> mApplicationInfoList;
 
     public ApplicationListFragment() {
         // Required empty public constructor
@@ -53,26 +38,84 @@ public class ApplicationListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_application_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_application_list, container, false);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        final PackageManager packageManager = activity.getPackageManager();
+        mApplicationInfoList = getApplicationList(packageManager);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Adapter adapter = new Adapter(getActivity(), R.layout.application_list_item);
+        adapter.addAll(mApplicationInfoList);
+        mGrid.setAdapter(adapter);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private List<ResolveInfo> getApplicationList(@NonNull PackageManager manager) {
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        // see PackageManager.MATCH_DEFAULT_ONLY
+        return manager.queryIntentActivities(intent, 0);
+    }
+
+    public static class Adapter extends ArrayAdapter<ResolveInfo> {
+
+        static class ViewHolder {
+            @InjectView(R.id.icon)
+            ImageView mIcon;
+            @InjectView(R.id.label)
+            TextView mLabel;
+
+            public ViewHolder(View view) {
+                ButterKnife.inject(this, view);
+            }
+        }
+
+        private int mLayoutResourceId;
+        private final PackageManager mManager;
+
+        public Adapter(Context context, @LayoutRes int resource) {
+            super(context, resource);
+            mLayoutResourceId = resource;
+            mManager = context.getPackageManager();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView != null) {
+                holder = (ViewHolder) convertView.getTag();
+            } else {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(mLayoutResourceId, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }
+
+            final ResolveInfo info = getItem(position);
+            holder.mIcon.setImageDrawable(info.loadIcon(mManager));
+            holder.mLabel.setText(info.loadLabel(mManager));
+
+            return convertView;
+        }
     }
 }
