2cd4edd04ce0e520e8863e872efd1d3f3c973557
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-04-25 23:23:54 +0900

    modify  - MainActivity layout.
    add
     - ApplicationListFragment, WidgetListFragment

--- Sample/HomeApplication/app/src/main/java/m/k/android/sample/homeapplication/MainActivity.java
+++ Sample/HomeApplication/app/src/main/java/m/k/android/sample/homeapplication/MainActivity.java
@@ -6,20 +6,37 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import m.k.android.sample.homeapplication.fragment.ApplicationListFragment;
+import m.k.android.sample.homeapplication.fragment.WidgetListFragment;
 
 public class MainActivity extends ActionBarActivity {
 
     private static final String TAG = "HomeApplication";
 
+    @InjectView(R.id.pager)
+    ViewPager mPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        mPager.setAdapter(new Adapter(getSupportFragmentManager()));
 
         final PackageManager packageManager = getPackageManager();
         List<ResolveInfo> applicationInfoList = getApplicationList(packageManager);
@@ -64,5 +81,29 @@ public class MainActivity extends ActionBarActivity {
 
     private void outputDebugLog(String msg) {
         Log.d(TAG, msg);
+    }
+
+    private class Adapter extends FragmentPagerAdapter {
+
+        public Adapter(@NonNull FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return new ApplicationListFragment();
+                case 1:
+                    return new WidgetListFragment();
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
     }
 }
