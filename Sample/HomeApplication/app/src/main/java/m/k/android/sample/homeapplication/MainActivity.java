7ece5f5599af7d135cba9ef2da4cf0d8c78354f0
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-04-26 01:18:11 +0900

    add  - show AppWidget list

--- Sample/HomeApplication/app/src/main/java/m/k/android/sample/homeapplication/MainActivity.java
+++ Sample/HomeApplication/app/src/main/java/m/k/android/sample/homeapplication/MainActivity.java
@@ -9,12 +9,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import java.util.List;
 
