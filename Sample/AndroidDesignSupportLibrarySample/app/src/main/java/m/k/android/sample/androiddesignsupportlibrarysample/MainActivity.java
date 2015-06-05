4677535a76e2b54b55dcf696337e66aac48dd97d
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-06-06 00:32:35 +0900

    add NavigationView sample.

--- Sample/AndroidDesignSupportLibrarySample/app/src/main/java/m/k/android/sample/androiddesignsupportlibrarysample/MainActivity.java
+++ Sample/AndroidDesignSupportLibrarySample/app/src/main/java/m/k/android/sample/androiddesignsupportlibrarysample/MainActivity.java
@@ -1,37 +1,46 @@
 package m.k.android.sample.androiddesignsupportlibrarysample;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((NavigationView)findViewById(R.id.navigation)).setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        boolean ret = true;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Toast.makeText(this, "onNavigationItemSelected : " + menuItem.getItemId(), Toast.LENGTH_SHORT).show();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(menuItem.getItemId()) {
+            case R.id.navigation_item_2:
+                break;
+            case R.id.navigation_item_3:
+                break;
+            case R.id.navigation_item_4:
+                break;
+            case R.id.navigation_item_5:
+                break;
+            case R.id.navigation_item_6:
+                break;
+            case R.id.navigation_item_7:
+                break;
+            default:
+                ret = false;
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        return ret;
     }
 }
