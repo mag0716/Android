79f417731d3726e9b8749c30c1aa3b3249ab0d03
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-06-20 12:38:19 +0900

    add view.

--- Sample/RecyclerViewSample/app/src/main/java/m/k/android/sample/recyclerviewsample/MainActivityFragment.java
+++ Sample/RecyclerViewSample/app/src/main/java/m/k/android/sample/recyclerviewsample/MainActivityFragment.java
@@ -2,14 +2,24 @@ package m.k.android.sample.recyclerviewsample;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    @InjectView(R.id.listview)
+    ListView mListView;
+    @InjectView(R.id.recyclerview)
+    RecyclerView mRecyclerView;
 
     public MainActivityFragment() {
     }
@@ -17,6 +27,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_simple_recyclerview, container, false);
+        View view = inflater.inflate(R.layout.fragment_simple_recyclerview, container, false);
+
+        ButterKnife.inject(this, view);
+
+        return view;
     }
 }
