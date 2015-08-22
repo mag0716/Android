f0092b040a3ef81a3b58ee110d197d7e57f84d51
Masashi Kishimoto <mkishimoto0716@gmail.com>
2015-08-22 16:24:09 +0900

    modify Preference mode.

--- Sample/ShareDataSample/app/src/main/java/m/k/android/sample/sharedatasample/DataChangeBroadcastReceiver.java
+++ Sample/ShareDataSample/app/src/main/java/m/k/android/sample/sharedatasample/DataChangeBroadcastReceiver.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
+/**
+ * 通知を受け取って、プリファレンスを書き換える
+ */
 public class DataChangeBroadcastReceiver extends BroadcastReceiver {
     public DataChangeBroadcastReceiver() {
     }
--- Sample/ShareDataSample/app/src/main/java/m/k/android/sample/sharedatasample/MainActivity.java
+++ Sample/ShareDataSample/app/src/main/java/m/k/android/sample/sharedatasample/MainActivity.java
@@ -41,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private SharedPreferences pref(Context context, String name) {
-        return context.getSharedPreferences(name, Context.MODE_PRIVATE);
+        return context.getSharedPreferences(name, Context.MODE_WORLD_READABLE);
     }
 }
