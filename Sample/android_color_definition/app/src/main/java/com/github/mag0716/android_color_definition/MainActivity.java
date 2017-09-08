7ed37d7542b52ffeb24619f4522fafcd4a060e53
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-09-08 22:27:05 +0900

    TimePickerDialog 追加

--- Sample/android_color_definition/app/src/main/java/com/github/mag0716/android_color_definition/MainActivity.java
+++ Sample/android_color_definition/app/src/main/java/com/github/mag0716/android_color_definition/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.View;
 import com.github.mag0716.android_color_definition.dialog.SampleAlert;
 import com.github.mag0716.android_color_definition.dialog.SimpleConfirmationDialog;
 import com.github.mag0716.android_color_definition.dialog.SimpleDatePickerDialog;
+import com.github.mag0716.android_color_definition.dialog.SimpleTimePickerDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -127,14 +128,15 @@ public class MainActivity extends AppCompatActivity {
                 dialog = new SampleAlert();
                 break;
             case R.id.button2:
+                dialog = new SimpleConfirmationDialog();
                 break;
             case R.id.button3:
-                dialog = new SimpleConfirmationDialog();
                 break;
             case R.id.button4:
                 dialog = new SimpleDatePickerDialog();
                 break;
             case R.id.button5:
+                dialog = new SimpleTimePickerDialog();
                 break;
         }
         if (dialog != null) {
