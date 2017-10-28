7bebad9a65a61af30696eb3f846536ddcd6e4aab
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-10-28 18:27:26 +0900

    AppCompat Custom Widgets

---
+++ AndroidLint/app/src/main/java/com/github/mag0716/androidlint/view/CustomButton.java
@@ -0,0 +1,22 @@
+package com.github.mag0716.androidlint.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by mag0716 on 2017/10/28.
+ */
+public class CustomButton extends Button {
+    public CustomButton(Context context) {
+        super(context);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
