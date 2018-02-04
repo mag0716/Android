2f57f18c682e4793910a2c48487e5adec082e81f
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-04 23:54:01 +0900

    Item を1件表示

---
+++ LibrarySample/GroupieSample/app/src/main/java/com/github/mag0716/groupiesample/Header.kt
@@ -0,0 +1,6 @@
+package com.github.mag0716.groupiesample
+
+/**
+ * Created by mag0716 on 2018/02/04.
+ */
+class Header(val title: String)
--- LibrarySample/GroupieSample/app/src/main/java/com/github/mag0716/groupiesample/MainActivity.kt
+++ LibrarySample/GroupieSample/app/src/main/java/com/github/mag0716/groupiesample/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.github.mag0716.groupiesample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.github.mag0716.groupiesample.databinding.HeaderBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import com.xwray.groupie.databinding.BindableItem
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var list: RecyclerView
+    val adapter = GroupAdapter<ViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        list = findViewById(R.id.list)
+        list.layoutManager = GridLayoutManager(this, adapter.spanCount)
+        list.adapter = adapter
+        createItems()
+    }
+
+    private fun createItems() {
+        /**
+         * Groups
+         *   Section
+         *   NestedGroup
+         *   ExpandableGroup
+         */
+
+        /**
+         * Items
+         */
+
+        adapter.add(HeaderItem(Header("header1")))
+    }
+
+    class HeaderItem constructor(private val header: Header) : BindableItem<HeaderBinding>() {
+        override fun bind(viewBinding: HeaderBinding, position: Int) {
+            viewBinding.header = header
+        }
+
+        override fun getLayout() = R.layout.header
+
     }
 }
