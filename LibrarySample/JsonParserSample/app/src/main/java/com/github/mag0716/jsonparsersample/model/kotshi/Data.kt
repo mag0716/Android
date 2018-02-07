bb262ab13a7f52041e4b2aff0150d99f17292629
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-07 23:16:26 +0900

    単純なパース処理を実装

---
+++ LibrarySample/JsonParserSample/app/src/main/java/com/github/mag0716/jsonparsersample/model/kotshi/Data.kt
@@ -0,0 +1,16 @@
+package com.github.mag0716.jsonparsersample.model.kotshi
+
+import com.squareup.moshi.Json
+import se.ansman.kotshi.JsonSerializable
+
+/**
+ * Created by mag0716 on 2018/02/07.
+ */
+@JsonSerializable
+data class Data(
+        val id: Int,
+        val text: String,
+        val nullableText: String?,
+        @Json(name = "custom_name")
+        val customName: String
+)
---
+++ LibrarySample/JsonParserSample/app/src/test/java/com/github/mag0716/jsonparsersample/model/kotshi/DataTest.kt
@@ -0,0 +1,77 @@
+package com.github.mag0716.jsonparsersample.model.kotshi
+
+import com.squareup.moshi.Moshi
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by mag0716 on 2018/02/07.
+ */
+class DataTest {
+
+    lateinit var moshi: Moshi
+
+    @Before
+    fun setUp() {
+        moshi = Moshi.Builder()
+                .add(ApplicationJsonAdapterFactory.INSTANCE)
+                .build()
+    }
+
+    @Test
+    fun 正常にパースされること() {
+        val json = """
+            {
+                "id":1,
+                "text":"text",
+                "nullableText":"nullableText",
+                "custom_name":"custom_name"
+            }
+            """.trimIndent()
+        val actual = moshi.adapter(Data::class.java).fromJson(json)
+        assertThat(actual, `is`(notNullValue()))
+        actual?.apply {
+            assertThat(id, `is`(1))
+            assertThat(text, `is`("text"))
+            assertThat(nullableText, `is`("nullableText"))
+            assertThat(customName, `is`("custom_name"))
+        }
+    }
+
+    @Test
+    fun Nullableな値がnullでも正常にパースされること() {
+        val json = """
+            {
+                "id":1,
+                "text":"text",
+                "nullableText":null,
+                "custom_name":"custom_name"
+            }
+            """.trimIndent()
+        val actual = moshi.adapter(Data::class.java).fromJson(json)
+        assertThat(actual, `is`(notNullValue()))
+        actual?.apply {
+            assertThat(id, `is`(1))
+            assertThat(text, `is`("text"))
+            assertThat(nullableText, `is`(nullValue()))
+            assertThat(customName, `is`("custom_name"))
+        }
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun NonNullな値がnullの場合は例外が発生すること() {
+        val json = """
+            {
+                "id":1,
+                "text":null,
+                "nullableText":"nullableText",
+                "custom_name":"custom_name"
+            }
+            """.trimIndent()
+        val actual = moshi.adapter(Data::class.java).fromJson(json)
+        fail("expect ")
+    }
+}
