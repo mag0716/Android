53f885db6087cfba1fd455fa6483b49554e8a0ab
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-17 20:00:17 +0900

    Android KTX でリファクタリング

--- LibrarySample/AndroidKtxSample/app/src/main/java/com/github/mag0716/androidktxsample/MainActivity.kt
+++ LibrarySample/AndroidKtxSample/app/src/main/java/com/github/mag0716/androidktxsample/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.github.mag0716.androidktxsample
 
 import android.graphics.Color
-import android.graphics.Typeface.BOLD
-import android.graphics.Typeface.ITALIC
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.*
+import android.text.SpannedString
+import android.text.style.TextAppearanceSpan
 import android.widget.TextView
+import androidx.text.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,14 +20,27 @@ class MainActivity : AppCompatActivity() {
         textView.text = generateSpannableStringBuilder()
     }
 
-    private fun generateSpannableStringBuilder(): SpannableStringBuilder {
-        return SpannableStringBuilder()
-                .append("normal\n")
-                .append("bold\n", StyleSpan(BOLD), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                .append("italic\n", StyleSpan(ITALIC), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                .append("underline\n", UnderlineSpan(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                .append("text color\n", ForegroundColorSpan(Color.RED), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                .append("background color\n", BackgroundColorSpan(Color.RED), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                .append("custom text appearance\n", TextAppearanceSpan(this, R.style.CustomTextAppearance), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    private fun generateSpannableStringBuilder(): SpannedString {
+        return buildSpannedString {
+            append("normal\n")
+            bold {
+                append("bold\n")
+            }
+            italic {
+                append("italic\n")
+            }
+            underline {
+                append("underline\n")
+            }
+            color(Color.RED) {
+                append("text color\n")
+            }
+            backgroundColor(Color.RED) {
+                append("background color\n")
+            }
+            inSpans(TextAppearanceSpan(this@MainActivity, R.style.CustomTextAppearance)) {
+                append("custom text appearance\n")
+            }
+        }
     }
 }
