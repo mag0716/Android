728cd922d50df80385be8823c68ed47e0e06451a
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-02-25 22:18:35 +0900

    ListPopupWindow の表示

--- Sample/PopupWindowSample/app/src/main/java/com/github/mag0716/popupwindowsample/MainActivity.kt
+++ Sample/PopupWindowSample/app/src/main/java/com/github/mag0716/popupwindowsample/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListPopupWindow
 import android.widget.PopupWindow
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var popup: PopupWindow
+    private lateinit var listPopup: ListPopupWindow
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,18 +40,35 @@ class MainActivity : AppCompatActivity() {
                 LayoutInflater.from(this).inflate(R.layout.view_popup, findViewById(R.id.container), false),
                 resources.getDimensionPixelOffset(R.dimen.popup_width),
                 resources.getDimensionPixelOffset(R.dimen.popup_height))
+        listPopup = ListPopupWindow(this)
+        listPopup.setAdapter(ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                listOf("Item1", "Item2", "Item3", "Item4", "Item5")
+        ))
     }
 
     override fun onPause() {
         super.onPause()
         popup.dismiss()
+        listPopup.dismiss()
     }
 
     fun onClick(view: View) {
-        if (popup.isShowing) {
-            popup.dismiss()
-        } else {
-            popup.showAsDropDown(view)
+        val id = view.id
+        when (id) {
+            R.id.popupButton -> {
+                if (popup.isShowing) {
+                    popup.dismiss()
+                } else {
+                    popup.showAsDropDown(view)
+                }
+            }
+            R.id.listPopupButton -> {
+                listPopup.anchorView = view
+                listPopup.show()
+            }
         }
+
     }
 }
