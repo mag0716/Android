cf3672395c15666c8b297f6ff740e56e425886ec
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-03-08 23:20:54 +0900

    RecyclerView を用意

--- Sample/RecyclerViewItemAnimationSample/app/src/main/java/com/github/mag0716/recyclerviewitemanimationsample/MainActivity.java
+++ Sample/RecyclerViewItemAnimationSample/app/src/main/java/com/github/mag0716/recyclerviewitemanimationsample/MainActivity.java
@@ -1,29 +1,30 @@
 package com.github.mag0716.recyclerviewitemanimationsample;
 
 import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewGroup container;
-    private View icon;
-    private TextView text;
+    private RecyclerView recyclerView;
     private ObjectAnimator animator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        container = findViewById(R.id.container);
-        icon = findViewById(R.id.icon);
-        text = findViewById(R.id.text);
-        text.setText("Text");
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new Adapter(this, 100));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         initAnimator();
     }
@@ -45,10 +46,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initAnimator() {
-        animator = ObjectAnimator.ofPropertyValuesHolder(icon,
-                PropertyValuesHolder.ofFloat("scaleX", 3f),
-                PropertyValuesHolder.ofFloat("scaleY", 3f));
-        animator.setDuration(5000);
-        animator.setRepeatCount(ObjectAnimator.INFINITE);
+//        animator = ObjectAnimator.ofPropertyValuesHolder(icon,
+//                PropertyValuesHolder.ofFloat("scaleX", 3f),
+//                PropertyValuesHolder.ofFloat("scaleY", 3f));
+//        animator.setDuration(5000);
+//        animator.setRepeatCount(ObjectAnimator.INFINITE);
+    }
+
+    private class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        private final LayoutInflater inflater;
+        private final int size;
+
+        public Adapter(@NonNull Context context, int size) {
+            inflater = LayoutInflater.from(context);
+            this.size = size;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(inflater.inflate(R.layout.item_cell, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.text.setText("Text" + position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return size;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView icon;
+            TextView text;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                icon = itemView.findViewById(R.id.icon);
+                text = itemView.findViewById(R.id.text);
+            }
+        }
     }
 }
