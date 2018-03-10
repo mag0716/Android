1e667a3039c34ea69df09c089136361ca3077ae9
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-03-11 00:25:55 +0900

    アニメーションを自前で計算して Canvas に描画するサンプルのための View を追加

---
+++ Sample/RecyclerViewItemAnimationSample/app/src/main/java/com/github/mag0716/recyclerviewitemanimationsample/AnimationCanvasView.java
@@ -0,0 +1,47 @@
+package com.github.mag0716.recyclerviewitemanimationsample;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by mag0716 on 2018/03/11.
+ */
+
+public class AnimationCanvasView extends View {
+
+    private Paint paint = new Paint();
+    private Bitmap bitmap;
+
+    public AnimationCanvasView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public AnimationCanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public AnimationCanvasView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+    }
+
+    private void init(@NonNull Context context) {
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+    }
+}
