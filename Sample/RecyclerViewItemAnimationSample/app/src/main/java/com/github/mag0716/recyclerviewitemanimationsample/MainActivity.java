61f8a61b78b6c602a8bd39e87eee71be6eaf0da8
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-03-21 17:21:09 +0900

    分かりやすいように区切り線を追加

--- Sample/RecyclerViewItemAnimationSample/app/src/main/java/com/github/mag0716/recyclerviewitemanimationsample/MainActivity.java
+++ Sample/RecyclerViewItemAnimationSample/app/src/main/java/com/github/mag0716/recyclerviewitemanimationsample/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         adapter = new Adapter(this, 100);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 
     @Override
