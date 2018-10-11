db622668ed99e3237b3f0845403cce645f923170
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-10-11 21:31:09 +0900

    getViewModel も試す

--- LibrarySample/KoinSample/viewmodel/src/main/java/com/github/mag0716/viewmodel/MainActivity.kt
+++ LibrarySample/KoinSample/viewmodel/src/main/java/com/github/mag0716/viewmodel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.mag0716.viewmodel
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import org.koin.android.viewmodel.ext.android.getViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "${mainViewModel.hello}($mainViewModel)")
+
+        val directMainViewModel = getViewModel<MainViewModel>()
+        Log.d(TAG, "${directMainViewModel.hello}($directMainViewModel)")
     }
 }
