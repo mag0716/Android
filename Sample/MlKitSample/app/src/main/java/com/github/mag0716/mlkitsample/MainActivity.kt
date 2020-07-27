48d657cb71f33dadb696e108dfa806a86cdf1d61
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-07-27 21:41:35 +0900

    クレジットカード番号、有効期限のパースを実装

--- Sample/MlKitSample/app/src/main/java/com/github/mag0716/mlkitsample/MainActivity.kt
+++ Sample/MlKitSample/app/src/main/java/com/github/mag0716/mlkitsample/MainActivity.kt
@@ -43,25 +43,29 @@ class MainActivity : AppCompatActivity() {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                    this,
-                    REQUIRED_PERMISSIONS,
-                    REQUEST_CODE_PERMISSIONS
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
             )
         }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 startCamera()
             }
         } else {
             Toast.makeText(
-                    this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
+                this,
+                "Permissions not granted by the user.",
+                Toast.LENGTH_SHORT
             ).show()
             finish()
         }
@@ -75,16 +79,17 @@ class MainActivity : AppCompatActivity() {
 
             preview = Preview.Builder().build()
 
-            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
             try {
                 cameraProvider.unbindAll()
 
                 camera = cameraProvider.bindToLifecycle(
-                        this,
-                        cameraSelector,
-                        preview,
-                        CreditCardAnalyzer.generateImageAnalysisUseCase(cameraExecutor)
+                    this,
+                    cameraSelector,
+                    preview,
+                    CreditCardAnalyzer.generateImageAnalysisUseCase(cameraExecutor)
                 )
                 preview?.setSurfaceProvider(viewFinder.createSurfaceProvider(camera?.cameraInfo))
             } catch (exception: Exception) {
@@ -95,8 +100,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-                baseContext,
-                it
+            baseContext,
+            it
         ) == PackageManager.PERMISSION_GRANTED
     }
 
@@ -105,12 +110,12 @@ class MainActivity : AppCompatActivity() {
         companion object {
             fun generateImageAnalysisUseCase(cameraExecutor: ExecutorService): ImageAnalysis {
                 return ImageAnalysis
-                        .Builder()
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                        .also {
-                            it.setAnalyzer(cameraExecutor, CreditCardAnalyzer())
-                        }
+                    .Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, CreditCardAnalyzer())
+                    }
             }
         }
 
@@ -122,15 +127,18 @@ class MainActivity : AppCompatActivity() {
             val mediaImage = imageProxy.image
             runBlocking {
                 if (mediaImage != null) {
-                    val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-                    recognizeCreditCard(image)
+                    val image =
+                        InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                    val creditCard = detectCreditCardInfo(recognizeCreditCard(image))
+                    Log.d(TAG, "analyze : $creditCard")
                 }
                 imageProxy.close()
             }
         }
 
-        private suspend fun recognizeCreditCard(input: InputImage): Text = suspendCoroutine { continuation ->
-            recognizer.process(input)
+        private suspend fun recognizeCreditCard(input: InputImage): Text =
+            suspendCoroutine { continuation ->
+                recognizer.process(input)
                     .addOnSuccessListener { visionText ->
                         Log.d(TAG, "success : $visionText")
                         continuation.resume(visionText)
@@ -139,6 +147,46 @@ class MainActivity : AppCompatActivity() {
                         Log.w(TAG, "failed", exception)
                         continuation.resumeWithException(exception)
                     }
+            }
+
+        private fun detectCreditCardInfo(visionText: Text): CreditCard? {
+            val textBlocks = visionText.textBlocks
+            var number: String? = null
+            var expiredDate: String? = null
+            for (block in textBlocks) {
+                if (number == null) {
+                    number = detectCreditCardNumber(block.text)
+                }
+                if (expiredDate == null) {
+                    expiredDate = detectCreditCardExpiredDate(block.text)
+                }
+                if (number != null && expiredDate != null) {
+                    return CreditCard(number, expiredDate)
+                }
+            }
+            return null
+        }
+
+        private fun detectCreditCardNumber(text: String): String? {
+            if (text.replace(" ", "")
+                    .matches(Regex("^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})\$"))
+            ) {
+                return text
+            }
+            return null
+        }
+
+        private fun detectCreditCardExpiredDate(text: String): String? {
+            //if(text.matches(Regex("[01][0-9]/[0-9]{2}"))) {
+            if (text.matches(Regex("[0-9]{2}/[0-9]{2}"))) {
+                return text
+            }
+            return null
         }
     }
 }
+
+data class CreditCard(
+    val number: String,
+    val expiredDate: String
+)
