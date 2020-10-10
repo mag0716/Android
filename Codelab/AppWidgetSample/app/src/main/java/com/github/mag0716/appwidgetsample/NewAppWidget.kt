add347f606eaa0426c8f5beb3d89d97aa3f275fb
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-10-10 21:30:11 +0900

    [Step4] AppWidgetProviderの実装を修正

--- Codelab/AppWidgetSample/app/src/main/java/com/github/mag0716/appwidgetsample/NewAppWidget.kt
+++ Codelab/AppWidgetSample/app/src/main/java/com/github/mag0716/appwidgetsample/NewAppWidget.kt
@@ -3,17 +3,24 @@ package com.github.mag0716.appwidgetsample
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.util.Log
 import android.widget.RemoteViews
 
 /**
  * Implementation of App Widget functionality.
  */
 class NewAppWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val TAG = "AppWidgetSample"
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        Log.d(TAG, "onUpdate : $appWidgetIds")
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -22,10 +29,12 @@ class NewAppWidget : AppWidgetProvider() {
 
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        Log.d(TAG, "onEnabled")
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+        Log.d(TAG, "onDisabled")
     }
 }
 
@@ -34,10 +43,9 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.new_app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.appwidget_id, appWidgetId.toString())
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
