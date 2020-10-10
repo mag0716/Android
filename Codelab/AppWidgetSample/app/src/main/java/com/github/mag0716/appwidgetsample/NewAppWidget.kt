0a6de9b79d9c7c3d04d4ad80911bac875389bb63
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-10-10 22:04:59 +0900

    [Step5] 定期的にウィジェットの表示内容を更新する処理を追加

--- Codelab/AppWidgetSample/app/src/main/java/com/github/mag0716/appwidgetsample/NewAppWidget.kt
+++ Codelab/AppWidgetSample/app/src/main/java/com/github/mag0716/appwidgetsample/NewAppWidget.kt
@@ -5,6 +5,11 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.util.Log
 import android.widget.RemoteViews
+import androidx.core.content.edit
+import com.github.mag0716.appwidgetsample.NewAppWidget.Companion.COUNT_KEY
+import com.github.mag0716.appwidgetsample.NewAppWidget.Companion.SHARED_PREFERENCES_FILE
+import java.text.DateFormat
+import java.util.*
 
 /**
  * Implementation of App Widget functionality.
@@ -13,6 +18,8 @@ class NewAppWidget : AppWidgetProvider() {
 
     companion object {
         private const val TAG = "AppWidgetSample"
+        const val SHARED_PREFERENCES_FILE = "appwidgetsample"
+        const val COUNT_KEY = "count"
     }
 
     override fun onUpdate(
@@ -45,8 +52,29 @@ internal fun updateAppWidget(
 ) {
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+
     views.setTextViewText(R.id.appwidget_id, appWidgetId.toString())
+
+    val prefs = context.getSharedPreferences(
+        SHARED_PREFERENCES_FILE,
+        Context.MODE_PRIVATE
+    )
+    val count = prefs.getInt(COUNT_KEY + appWidgetId, 0)
+    val newCount = count.inc()
+    val dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(Date())
+    views.setTextViewText(
+        R.id.appwidget_update,
+        context.resources.getString(
+            R.string.date_count_format,
+            newCount,
+            dateString
+        )
+    )
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+
+    prefs.edit {
+        putInt(COUNT_KEY + appWidgetId, count)
+    }
 }
