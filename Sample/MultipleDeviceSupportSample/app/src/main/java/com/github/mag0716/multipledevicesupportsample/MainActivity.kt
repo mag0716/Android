fc75c1e240a7aba930abac0755e2de072bf2abca
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-10-22 21:33:39 +0900

    SettingsActivityへの遷移処理追加

--- Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/MainActivity.kt
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.github.mag0716.multipledevicesupportsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mag0716.multipledevicesupportsample.ui.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+            }
+        }
+        return false
     }
 }
--- Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/settings/SettingsActivity.kt
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/settings/SettingsActivity.kt
@@ -16,9 +16,8 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
-
+    
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
