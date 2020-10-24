6bcd130421228130c25d333d2d14d5f7df84bfd6
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-10-24 20:20:05 +0900

    CameraFragmentへの遷移処理

--- Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/detail/DetailFragment.kt
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/detail/DetailFragment.kt
@@ -1,11 +1,10 @@
 package com.github.mag0716.multipledevicesupportsample.ui.detail
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.github.mag0716.multipledevicesupportsample.R
 
@@ -18,6 +17,7 @@ class DetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
@@ -26,5 +26,19 @@ class DetailFragment : Fragment() {
         val item = args.item
         view.findViewById<TextView>(R.id.contentText).text = item.content
         view.findViewById<TextView>(R.id.detailText).text = item.details
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_detail, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.camera -> {
+                findNavController().navigate(R.id.navigateToCamera)
+            }
+        }
+        return false
     }
 }
