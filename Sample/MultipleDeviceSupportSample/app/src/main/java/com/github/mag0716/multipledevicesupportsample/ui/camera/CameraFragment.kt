0053734549aeaee9125b26778032d0059e4371a5
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-10-29 21:06:03 +0900

    カメラでの撮影処理実装

--- Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/camera/CameraFragment.kt
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/camera/CameraFragment.kt
@@ -1,13 +1,38 @@
 package com.github.mag0716.multipledevicesupportsample.ui.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.github.mag0716.multipledevicesupportsample.R
+import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
+
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    private val executor = Executors.newSingleThreadExecutor()
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var previewView: PreviewView
+    private lateinit var captureButton: ImageButton
+
+    private var camera: Camera? = null
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +40,115 @@ class CameraFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        previewView = view.findViewById(R.id.previewView)
+        previewView.implementationMode = PreviewView.ImplementationMode.PERFORMANCE
+        captureButton = view.findViewById(R.id.capture_button)
+        captureButton.setOnClickListener {
+            capture()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (allPermissionsGranted()) {
+            previewView.post { startCamera() }
+        } else {
+            requestPermissions(
+                REQUIRED_PERMISSIONS,
+                REQUEST_CAMERA_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (allPermissionsGranted()) {
+                previewView.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Permissions not granted by user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(), permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun startCamera() {
+        val cameraSelector =
+            CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+        // setup preview
+        val previewBuilder = Preview.Builder().apply {
+            setTargetAspectRatio(AspectRatio.RATIO_16_9)
+        }
+        val preview = previewBuilder.build()
+        preview.setSurfaceProvider(
+            previewView.surfaceProvider
+        )
+
+        // setup imagecapture
+        imageCapture = ImageCapture.Builder().apply {
+            setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+        }.build()
+
+        val cameraProvider = cameraProviderFuture.get()
+        cameraProvider.unbindAll()
+        camera = cameraProvider.bindToLifecycle(
+            this,
+            cameraSelector,
+            preview,
+            imageCapture
+        )
+    }
+
+    private fun capture() {
+        val file = File(requireContext().filesDir, "${System.currentTimeMillis()}.jpg")
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(file)
+            .build()
+        imageCapture?.takePicture(outputOptions,
+            executor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val msg = "Photo capture succeeded: ${file.absolutePath}"
+                    previewView.post {
+                        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                        // TODO: 画面遷移
+                    }
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    val msg = "Photo capture failed : $exception"
+                    previewView.post {
+                        Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
     }
 }
