a72b90961dd5637f5762f53f6a013627903dbefe
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-11-01 16:46:13 +0900

    MainActivityのパッケージを移動

--- Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/MainActivity.kt
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.github.mag0716.multipledevicesupportsample
+package com.github.mag0716.multipledevicesupportsample.ui
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mag0716.multipledevicesupportsample.R
 import com.github.mag0716.multipledevicesupportsample.ui.license.LicenseActivity
 import com.github.mag0716.multipledevicesupportsample.ui.settings.SettingsActivity
 
