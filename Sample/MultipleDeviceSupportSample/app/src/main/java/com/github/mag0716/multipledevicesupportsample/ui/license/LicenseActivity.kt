b2318caf436edb4fbe8b6907e2eb5efd2edcc809
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-11-01 11:33:31 +0900

    ライセンス表記のための画面を追加

---
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/license/LicenseActivity.kt
@@ -0,0 +1,12 @@
+package com.github.mag0716.multipledevicesupportsample.ui.license
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.mag0716.multipledevicesupportsample.R
+
+class LicenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+    }
+}
