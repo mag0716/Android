f1871a419ffa4fe25ac4eb52c777b42cf50c384b
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-11-01 16:31:00 +0900

    assetにあるhtmlファイルを読み込む処理を追加

--- Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/license/LicenseActivity.kt
+++ Sample/MultipleDeviceSupportSample/app/src/main/java/com/github/mag0716/multipledevicesupportsample/ui/license/LicenseActivity.kt
@@ -1,12 +1,44 @@
 package com.github.mag0716.multipledevicesupportsample.ui.license
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mag0716.multipledevicesupportsample.R
 
 class LicenseActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_license)
+
+        webView = findViewById(R.id.web_view)
+
+        if (savedInstanceState == null) {
+            webView.loadUrl("file:///android_asset/licenses.html")
+        } else {
+            webView.restoreState(savedInstanceState)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        webView.destroy()
     }
 }
