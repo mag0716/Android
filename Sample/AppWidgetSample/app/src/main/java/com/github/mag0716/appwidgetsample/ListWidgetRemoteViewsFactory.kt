95dc4ae91b14f9890b969c68ab71d89586a05c52
Masashi Kishimoto <mkishimoto0716@gmail.com>
2021-04-02 21:30:15 +0900

    ListViewを扱うAppWidgetを追加

---
+++ Sample/AppWidgetSample/app/src/main/java/com/github/mag0716/appwidgetsample/ListWidgetRemoteViewsFactory.kt
@@ -0,0 +1,65 @@
+package com.github.mag0716.appwidgetsample
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+
+class ListWidgetRemoteViewsFactory(
+    val context: Context,
+    val intent: Intent
+) : RemoteViewsService.RemoteViewsFactory {
+
+    companion object {
+        private const val TAG = "RemoteViewsFactory"
+    }
+
+    private val titleList = mutableListOf<String>()
+
+    override fun onCreate() {
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onDataSetChanged() {
+        Log.d(TAG, "onDataSetChanged")
+        val list = fetchData()
+        titleList.clear()
+        titleList.addAll(list)
+    }
+
+    override fun onDestroy() {
+        titleList.clear()
+    }
+
+    override fun getCount(): Int {
+        Log.d(TAG, "getCount : ${titleList.size}")
+        return titleList.size
+    }
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val remoteViews = RemoteViews(context.packageName, R.layout.list_item)
+        remoteViews.setTextViewText(R.id.title, titleList[position])
+        return remoteViews
+    }
+
+    override fun getLoadingView() = null
+
+    override fun getViewTypeCount() = 1
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    override fun hasStableIds() = true
+
+    private fun fetchData(): List<String> {
+        Thread.sleep(5000)
+
+        val currentTimeMillis = System.currentTimeMillis()
+
+        val list = mutableListOf<String>()
+        for (i in 0..10) {
+            list.add("Title$i($currentTimeMillis)")
+        }
+        return list
+    }
+}
---
+++ Sample/AppWidgetSample/app/src/main/java/com/github/mag0716/appwidgetsample/ListWidgetService.kt
@@ -0,0 +1,17 @@
+package com.github.mag0716.appwidgetsample
+
+import android.content.Intent
+import android.util.Log
+import android.widget.RemoteViewsService
+
+class ListWidgetService : RemoteViewsService() {
+
+    companion object {
+        private const val TAG = "RemoteViewsService"
+    }
+
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        Log.d(TAG, "onGetViewFactory")
+        return ListWidgetRemoteViewsFactory(applicationContext, intent)
+    }
+}
