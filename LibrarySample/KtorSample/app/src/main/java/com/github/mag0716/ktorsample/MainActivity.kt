7300fb32a5a9d5abac663f3f4eb37162cf52695d
mag0716 <mkishimoto0716@gmail.com>
2022-03-26 22:00:28 +0900

    ktorでAPIのリクエスト、レスポンス表示まで実装

--- LibrarySample/KtorSample/app/src/main/java/com/github/mag0716/ktorsample/MainActivity.kt
+++ LibrarySample/KtorSample/app/src/main/java/com/github/mag0716/ktorsample/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.github.mag0716.ktorsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.github.mag0716.ktorsample.api.ApiClient
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            lifecycleScope.launchWhenStarted {
+                val apiClient = ApiClient()
+                val repositoryList = apiClient.repos("mag0716")
+                textView.text = repositoryList.toString()
+            }
+        }
     }
 }
